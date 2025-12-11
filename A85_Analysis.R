6dfb31dda0e0fe8bb85dfc3cadbc4b3a0a6ccde5
Rahul Kolupoti <rk25acs@herts.ac.uk>
2025-12-11 20:21:03 +0000

    Organised current code

--- A85_Analysis.R
+++ A85_Analysis.R
@@ -1,4 +1,27 @@
+#Statistical analysis
+max(gpa_iq$gpa)
+max(gpa_iq$iq)
+
+min(gpa_iq$gpa)
+min(gpa_iq$iq)
+
+mean(gpa_iq$gpa)
+mean(gpa_iq$iq)
+
+length(which(gpa_iq$gender==1))
+length(which(gpa_iq$gender==2))
+
+mean(gpa_iq$gpa)
+sd(gpa_iq$gpa)
+
+mean(gpa_iq$iq)
+sd(gpa_iq$iq)
+
+#Pearson Test with significance
+
 print(cor.test(gpa_iq$gpa,gpa_iq$iq, method = "pearson"))
+
+#Plotting Scatterplot graph
 
 plot(x = gpa_iq$iq, y = gpa_iq$gpa,
      main = "Scatterplot of GPA vs. IQ",  
@@ -6,12 +29,3 @@ plot(x = gpa_iq$iq, y = gpa_iq$gpa,
      ylab = "GPA",                       
      pch = 19,                           
      col = "blue")
-
-max(gpa_iq$gpa)
-max(gpa_iq$iq)
-min(gpa_iq$gpa)
-min(gpa_iq$iq)
-mean(gpa_iq$gpa)
-mean(gpa_iq$iq)
-length(which(gpa_iq$gender==1))
-length(which(gpa_iq$gender==2))
